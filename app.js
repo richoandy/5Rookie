6188d477cc38b542760a02b573176cff6d4b9a09
Zena <zena.vilenia@gmail.com>
2018-03-27 17:22:00 +0700

    add relation team to user

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const bodyParser = require('body-parser')
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function(req, res){
-  res.send('5Rookie')
+//index
+var routeIndex = require('./routes/index.js')
+app.use('/', routeIndex)
+
+//user
+var routeUser = require('./routes/user.js')
+app.use('/home', routeUser)
+
+//test views
+
+app.get('/createnewteam', function(req, res){
+  res.render('my_team')
+})
+
+app.get('/createnewteam', function(req, res){
+  res.render('my_team')
 })
 
 app.listen(3800, () => console.log('express running'))
