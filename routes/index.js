abeb2ad98218ca90b84c35dcadeedb9dd5aaeafd
Zena <zena.vilenia@gmail.com>
2018-03-28 14:55:02 +0700

    register

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
+const cUser = require('../controllers/ControllerUser.js');
 
 router.get('/', (req, res) =>
   res.send("INDEX 5 ROOKIE")
@@ -14,5 +15,24 @@ router.get('/register', (req, res) =>
   res.render("register")
 )
 
+router.post('/register', function(req, res) {
+  let attributeUser = {
+    nickname: req.body.nickname,
+    password: req.body.password,
+    salt: "-",
+    email: req.body.email,
+    position: req.body.position,
+    medal: req.body.medal,
+    star: req.body.star,
+    link_steam: req.body.link_steam,
+  };
+  cUser.add(attributeUser)
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+});
 
 module.exports = router
