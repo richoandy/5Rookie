554285e4880fd115f6679f0e093ded35da5cb659
Zena <zena.vilenia@gmail.com>
2018-03-28 16:19:42 +0700

    my team

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
-
+const cUser = require('../controllers/ControllerUser.js');
+const model = require('../models');
 
 router.get('/', (req, res) =>
   res.render('home.ejs')
@@ -8,6 +9,22 @@ router.get('/', (req, res) =>
 
 router.get('/profile', (req, res) =>
   res.send("profile page")
+)
+
+router.get('/teams', (req, res) =>
+  model.User.findOne({
+    where: {
+      id: 7
+    },
+    include: [
+      {
+        model: model.UserTeam,
+      }
+    ]
+  })
+  .then(user => {
+    res.render('my_team', {user});
+  })
 )
 
 module.exports = router
