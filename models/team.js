a6bda8c643f0b0200616a281ed209a858e891b51
Zena <zena.vilenia@gmail.com>
2018-03-28 15:09:41 +0700

    edit package-lock.json

--- models/team.js
+++ models/team.js
@@ -4,36 +4,35 @@ module.exports = (sequelize, DataTypes) => {
     nama: DataTypes.STRING,
     id_ketua: DataTypes.INTEGER
   }, {
-    hooks: {
-      afterCreate(team, options){
-        models.UserTeam.create({
-          teamId = team.id,
-          userId = 0,
-          position = "1"
-        })
-        models.UserTeam.create({
-          teamId = team.id,
-          userId = 0,
-          position = "2"
-        })
-        models.UserTeam.create({
-          teamId = team.id,
-          userId = 0,
-          position = "3"
-        })
-        models.UserTeam.create({
-          teamId = team.id,
-          userId = 0,
-          position = "4"
-        })
-        models.UserTeam.create({
-          teamId = team.id,
-          userId = 0,
-          position = "5"
-        })
-      }
-    }
-  }
+    // hooks: {
+    //   afterCreate(team, options){
+    //     models.UserTeam.create({
+    //       teamId = team.id,
+    //       userId = 0,
+    //       position = "1"
+    //     })
+    //     models.UserTeam.create({
+    //       teamId = team.id,
+    //       userId = 0,
+    //       position = "2"
+    //     })
+    //     models.UserTeam.create({
+    //       teamId = team.id,
+    //       userId = 0,
+    //       position = "3"
+    //     })
+    //     models.UserTeam.create({
+    //       teamId = team.id,
+    //       userId = 0,
+    //       position = "4"
+    //     })
+    //     models.UserTeam.create({
+    //       teamId = team.id,
+    //       userId = 0,
+    //       position = "5"
+    //     })
+    //   }
+    // }
   });
   Team.associate = function(models) {
     // associations can be defined here
